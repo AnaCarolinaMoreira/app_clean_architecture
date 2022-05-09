6ead1e452a9d70a9819aef5bee299de7f0052744
ana.moreira <anacarolina35980@gmail.com>
2022-05-09 13:45:25 -0300

    criada camada presenter

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -3,21 +3,24 @@ import 'package:app_clean_architecture/modules/search/domain/repositories/search
 import 'package:app_clean_architecture/modules/search/domain/usecases/search_by_text.dart';
 import 'package:app_clean_architecture/modules/search/external/datasources/github_datasources.dart';
 import 'package:app_clean_architecture/modules/search/infra/repositories/search_repository_impl.dart';
+import 'package:app_clean_architecture/modules/search/presenter/search_bloc.dart';
+import 'package:app_clean_architecture/modules/search/presenter/search_page.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class AppModule extends MainModule {
+class AppModule extends Module {
   @override
   List<Bind> get binds => [
         Bind((i) => Dio()),
         Bind((i) => GithubSearchDatasource(i())),
         Bind((i) => SearchRepositoryImpl(i())),
         Bind((i) => SearchByTextImpl(i())),
+        Bind((i) => SearchBloc(i())),
       ];
 
   @override
-  List get routers => throw UnimplementedError();
+  List<ModularRoute> get routes => [ChildRoute('/', child: (context, args) => SearchPage())];
 
   @override
   Widget get bootstrap => AppWidget();
--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -6,12 +7,22 @@ class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+      title: "FLutter demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      supportedLocales: [const Locale('pt', 'BR')],
+      debugShowCheckedModeBanner: false,
+    ); /*MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
-    );
+      initialRoute: Modular.initialRoute,
+      debugShowCheckedModeBanner: false,
+    );*/
   }
 }
